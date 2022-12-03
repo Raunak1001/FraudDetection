3da0865043d06be47637c0ba8da09b6a23e1626f
Raunak1001 <raunak@epifi.com>
2022-12-03 18:02:24 +0530

    Adding twitter scrapper

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ if __name__ == "__main__":
     
     if TEST_ENV:
         ch = 1
-        fromDate = '2022-11-02'
-        toDate = '2022-12-02'
+        # fromDate = '2022-11-02'
+        # toDate = '2022-12-02'
     else:    
         ch = int(input())
     if ch == 1:
         tweets_df = scrapper.get_tweets(keywords1=common.KEYWORDS, keywords2=['number','mobile'],from_time=fromDate, to_time=toDate, n=100)
         print("********** Got tweets ********")
         extracted_df = data_converter.get_fraud_data(tweets_df)
-        print(extracted_df)
+        print("**** phone numbers ***** {0}".format(extracted_df))
         final_df = processor.process_data(extracted_df)
-        print(final_df)
+        print("**** final result ***** {0}".format(extracted_df))
