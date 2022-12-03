bee107a8b9415ad0c8a652d4ad5705900f1c2af4
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Pro-2.local>
2022-12-03 16:32:50 +0530

    added main

--- common.py
+++ common.py
@@ -3,10 +3,17 @@ IMAGE_URL = "Image Url"
 SHARE_COUNT = "Share Count"
 LIKE_COUNT = "Like Count"
 REPLY_COUNT = "Reply Count"
-USERNMAE = "User Name"
+USERNAME = "User Name"
 PLATFROM = "Platform"
 
 TWITTER = "Twitter"
 
 OPERATOR_OR = 'OR'
 OPERATOR_AND = 'AND'
+KEYWORDS = ["fraud", "scam", "fraudster", "scammer", "cheat", "loot", "scamster", "cheater", "looter"]
+USERNAMES = ["@CPMumbaiPolice", "@cyberdost", "@noidapolice", "@uppolice", "@delhipolice", "@cybercrimeCID", "@mumbaipolice", "@dgpMaharashtra", "@mahaPolice", "@blrCityPolice", "@cybercellRaj", "@cybercellindia", "@DoT_India", "@assampolice", "@wbpolice", "@olkatapolice"]
+TYPE = 'Type'
+VALUE = 'Value'
+SOURCE = 'Source'
+SCORE = 'Score'
+
