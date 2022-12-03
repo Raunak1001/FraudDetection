2bc7a77ad22eb3e2a96ded89fbcbc37499845efd
ankit <ankitchahar11@gmail.com>
2022-12-03 13:30:43 +0530

    change requirements

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
 if __name__ == "__main__":
-    print("main function")
+    # Created a list to append all tweet attributes(data)
+    attributes_container = []
+    query = '(phone OR number OR mobile OR no OR bank )(fraudster OR scammer OR cheat OR scam OR fraud OR loot ) (@CPMumbaiPolice  OR @cyberdost OR @noidapolice OR @uppolice OR @delhipolice OR @cybercrimeCID OR @mumbaipolice OR @dgpMaharashtra OR @mahaPolice OR @blrCityPolice OR @cybercellRaj OR @cybercellindia OR @DoT_India  OR @assampolice OR @wbpolice OR @kolkatapolice)'
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+        if i>100:
+            break
+        attributes_container.append([tweet.date, tweet.content])
+        
+    # Creating a dataframe from the tweets list above 
+    tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Tweets"])
+    print(tweets_df)
