9a9dfb765b543c1ef136ed1aa3553223b85ce8cf
ankit <ankitchahar11@gmail.com>
2022-12-03 17:49:14 +0530

    add test env

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from extractor.twitter import data_converter
 from processor.twitter import processor
 import common
 
+fromDate = ''
+toDate = ''
+TEST_ENV = True
+
 if __name__ == "__main__":
     print("____menu_____")
     print("1: to find user generated fraudulent data")
     print("2: to find fraudulent data for specific identifier")
     
-    ch = int(input())
     
+    if TEST_ENV:
+        ch = 1
+        fromDate = '2022-11-02'
+        toDate = '2022-12-02'
+    else:    
+        ch = int(input())
     if ch == 1:
-        fromDate = ''
-        toDate = ''
         tweets_df = scrapper.get_tweets(keywords1=common.KEYWORDS, keywords2=['number','mobile'],from_time=fromDate, to_time=toDate, n=100)
         print("********** Got tweets ********")
         extracted_df = data_converter.get_fraud_data(tweets_df)
