5b8fffbb9153b6f016e5bba2454fbb2c693d87f4
Raunak1001 <raunak@epifi.com>
2022-12-04 16:45:17 +0530

    Adding parallel date fetch

--- main.py
+++ main.py
@@ -4,43 +4,77 @@ from processor.twitter import processor
 import common
 import pandas as pd
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+import datetime
+from multiprocessing import Process
+import time
 
-fromDate = ''
-toDate = ''
+warnings.simplefilter(action='ignore', category=FutureWarning)
 TEST_ENV = True
+
+
+def detect_fraud(count, fromDate, toDate, n):
+    tweets_df = scrapper.get_tweets(keywords1=common.KEYWORDS, keywords2=['number', 'mobile', 'phone', 'no', 'ph'],
+                                    from_time=fromDate, to_time=toDate, n=n,
+                                    operator=common.OPERATOR_AND, mentions=common.MENTIONS)
+    print("********** Got tweets ********")
+    tweets_df = scrapper.fill_media_content(tweets_df)
+    tweets_df.to_csv('raw_data.csv')
+    print("******** Extracted media ******")
+    extracted_df = data_converter.get_fraud_data(tweets_df)
+    extracted_df.to_csv('extracted_data.csv')
+    print("******** Extracted data ********")
+    final_df = processor.process_data(extracted_df)
+    final_df = final_df.sort_values(by=[common.FINAL_SCORE], ascending=False)
+    final_df.to_csv('file-' + str(count) + '.csv')
+    print("****** Final output ready ******")
+
 
 if __name__ == "__main__":
     print("____menu_____")
-    print("1: to find user generated fraudulent data")
-    print("2: to find fraudulent data for specific identifier")
-
-
-    if TEST_ENV:
-        ch = 1
-        # fromDate = '2022-11-02'
-        # toDate = '2022-12-02'
-    else:
-        print("____menu_____")
-        print("1: to find user generated fraudulent data")
-        print("2: to find fraudulent data for specific identifier")
-        ch = int(input())
-    if ch == 1:
-        tweets_df = scrapper.get_tweets(keywords1=common.KEYWORDS, keywords2=['number', 'mobile', 'phone', 'no', 'ph'],
-                                        from_time=fromDate, to_time=toDate, n=common.TWEET_COUNT,
-                                        operator=common.OPERATOR_AND, mentions=common.MENTIONS)
-        print("********** Got tweets ********")
-        tweets_df = scrapper.fill_media_content(tweets_df)
-        tweets_df.to_csv('raw_data.csv')
-        print("******** Extracted media ******")
-        extracted_df = data_converter.get_fraud_data(tweets_df)
-        extracted_df.to_csv('extracted_data.csv')
-        print("******** Extracted data ********")
-        final_df = processor.process_data(extracted_df)
-        final_df = final_df.sort_values(by=[common.FINAL_SCORE], ascending=False)
-        final_df.to_csv('file1.csv')
-        print("****** Final output ready ******")
+    print("1: number of tweets fo consider for analysis: ")
+    print("2: date range detection")
+    print("3: targeted fraud detection")
+    ch = int(input())
+    if ch==1:
+        n = input("number of tweets: ")
+        detect_fraud(0, None, None, n)
     if ch == 2:
+        fromDate = input("enter from date: ")
+        toDate = input("enter to date: ")
+        from_time = datetime.datetime.strptime(fromDate, '%Y-%m-%d').date()
+        to_time = datetime.datetime.strptime(toDate, '%Y-%m-%d').date()
+        new_to_time = from_time
+        count = 0
+        i = 0
+        threads = []
+        while new_to_time < to_time:
+            i += 1
+            count = count + 1
+            new_to_time = from_time + datetime.timedelta(days=2)
+            threads.append(Process(target=detect_fraud,
+                                   args=(i, from_time.strftime('%Y-%m-%d'), new_to_time.strftime('%Y-%m-%d'), common.TWEET_COUNT,)))
+            if new_to_time > to_time:
+                new_to_time = to_time
+            if count % 10 == 0:
+                count = 0
+                for thread in threads:
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+                time.sleep(0.25)
+                threads.clear()
+            from_time = new_to_time
+
+        count = 0
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+        time.sleep(0.25)
+
+    if ch == 3:
         phone_number = input("please enter the phone number to check: ")
         result_data = [[common.PHONE_NUMBER, phone_number, common.TWITTER, 0, '', 0, 0, 0]]
         final_df = processor.process_data(pd.DataFrame(result_data,
